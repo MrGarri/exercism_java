34f0b7b90c3db163cf0764a0f0dbd4140cdaeecc
Roberto Garrido <garrihendrix@gmail.com>
2019-03-12 16:52:00 +0100

    Add run length encoding

---
+++ run-length-encoding/src/main/java/RunLengthEncoding.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RunLengthEncoding {
+
+    String encode(String input) {
+        
+    }
+
+    String decode(String input) {
+
+    }
+
+}
