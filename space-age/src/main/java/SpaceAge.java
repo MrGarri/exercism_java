068e8549c0af6b31defa1c06bd5628bbb899f175
Roberto Garrido <garrihendrix@gmail.com>
2019-04-27 10:50:23 +0200

    Remove unneeded formatTo2Decimals function

--- space-age/src/main/java/SpaceAge.java
+++ space-age/src/main/java/SpaceAge.java
@@ -17,40 +17,35 @@ class SpaceAge {
     }
 
     double onEarth() {
-        return formatTo2Decimals(seconds / SECONDS_OF_YEAR_ON_EARTH);
+        return seconds / SECONDS_OF_YEAR_ON_EARTH;
     }
 
     double onMercury() {
-        return formatTo2Decimals(onEarth() / MERCURY_MULTIPLIER);
+        return onEarth() / MERCURY_MULTIPLIER;
     }
 
     double onVenus() {
-        return formatTo2Decimals(onEarth() / VENUS_MULTIPLIER);
+        return onEarth() / VENUS_MULTIPLIER;
     }
 
     double onMars() {
-        return formatTo2Decimals(onEarth() / MARS_MULTIPLIER);
+        return onEarth() / MARS_MULTIPLIER;
     }
 
     double onJupiter() {
-        return formatTo2Decimals(onEarth() / JUPITER_MULTIPLIER);
+        return onEarth() / JUPITER_MULTIPLIER;
     }
 
     double onSaturn() {
-        return formatTo2Decimals(onEarth() / SATURN_MULTIPLIER);
+        return onEarth() / SATURN_MULTIPLIER;
     }
 
     double onUranus() {
-        return formatTo2Decimals(onEarth() / URANUS_MULTIPLIER);
+        return onEarth() / URANUS_MULTIPLIER;
     }
 
     double onNeptune() {
-        return formatTo2Decimals(onEarth() / NEPTUNE_MULTIPLIER);
-    }
-
-    private double formatTo2Decimals(double num) {
-        int round = (int) Math.round(num * 100);
-        return round / 100.0;
+        return onEarth() / NEPTUNE_MULTIPLIER;
     }
 
 }
